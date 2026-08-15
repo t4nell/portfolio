8cd7fc8552be1694be13d19c8b1a42c1e47d5d12
Michel Korfmacher <michelkorfmacher@yahoo.de>
2026-08-15 23:59:22 +0200

    feat: add header component with responsive design and global styles

---
+++ src/app/layout/header/header.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [NgOptimizedImage],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Header {}
