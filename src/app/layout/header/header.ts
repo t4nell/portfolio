52c8f29a7d8587b1bf9f49f090120c208c9c7a0b
t4nel <michelkorfmacher@yahoo.de>
2026-08-17 17:04:03 +0200

    feat: add app hero layout shell, use routerLink in header, and consolidate reset styles

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
