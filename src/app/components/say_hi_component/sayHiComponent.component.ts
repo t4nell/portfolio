37bf0348aefa31d21931da693ceb4111e6ed6fab
t4nel <michelkorfmacher@yahoo.de>
2026-09-17 16:30:41 +0200

    refactor: replace footer section in Say Hi component with reusable Footer component; remove associated styles

--- src/app/components/say_hi_component/sayHiComponent.component.ts
+++ src/app/components/say_hi_component/sayHiComponent.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Footer } from '../../layout/footer/footer';
 
 @Component({
   selector: 'app-say-hi-component',
+  imports: [Footer],
   templateUrl: './sayHiComponent.component.html',
   styleUrl: './sayHiComponent.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
